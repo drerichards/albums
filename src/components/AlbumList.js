7d4a8d332c9f2f461c27164e6953b708acd8bcd2
Andre Richardson <g.andrerichards@gmail.com>
2018-06-21 07:41:32 -0400

    fetch api data to app

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Text, View} from 'react-native';
+import axios from 'axios';
 
-const AlbumList = props => {
-    return (
-        <View>
-            <Text>AlbumList</Text>
-        </View>
-    );
-};
+import AlbumDetail from './AlbumDetail'
+
+class AlbumList extends Component {
+    state = {albums: {}}
+
+    componentDidMount(){
+        axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+        .then(response => {
+            this.setState({albums: response.data})
+        })
+    }
+
+    renderAlbums(){
+
+    }
+
+    render() {
+        console.log(this.state)
+        return (
+            <View>
+                <Text>Album List</Text>
+            </View>
+        );
+    }
+}
 
 export default AlbumList;

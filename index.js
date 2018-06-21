707ab7e03fc2d0f3b98a3a66a0ac072b24d29aab
Andre Richardson <g.andrerichards@gmail.com>
2018-06-21 06:19:47 -0400

    set up views for album components

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import {Text, AppRegistry} from 'react-native';
+import {Text, View, AppRegistry} from 'react-native';
 import Header from './src/components/Header'
+import AlbumList from './src/components/AlbumList'
 
 const App = () => {
     return(
+        <View>
             <Header headerText={'Albums'}/>
+            <AlbumList/>
+        </View>
     )
 }
 AppRegistry.registerComponent('albums', () => App);
---
+++ src/components/AlbumDetail.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const AlbumDetail = props => {
+    return (
+        <View>
+            <Text>Album Detail</Text>
+        </View>
+    );
+};
+
+export default AlbumDetail;
